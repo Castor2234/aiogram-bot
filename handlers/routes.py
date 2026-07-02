1060b92698c7e40a7a7b2f94934f457628f1d194
Castor2234 <castor2234@gmail.com>
2026-07-02 15:04:33 +0300

    small commit

--- handlers/routes.py
+++ handlers/routes.py
@@ -4,6 +4,8 @@ from aiogram.filters import Command, callback_data
 from aiogram.types import (
     Message,
     CallbackQuery,
+    ReplyKeyboardMarkup,
+    KeyboardButton,
     FSInputFile,
     InputMediaPhoto
 )
@@ -15,6 +17,7 @@ ADM_IDS=getenv('ADMIN_ID')
 
 from handlers.inline_keyboards import *
 import aiosqlite
+
 
 router = Router()
 
